474e9171c62bc2e665da0d081ec19c2ed3381c64
nkanja <nelsonkjasonk@gmail.com>
2024-11-25 16:15:34 +0300

    changing variants

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -32,7 +32,7 @@ const ExperienceCard = ({technologie}) => {
      </div>
 
         <ul className='mt-5 list-disc ml-5 space-y-2'>
-          {technologie  .points.map((point, index) => (
+          {technologie.points.map((point, index) => (
             <li
               key={`experience-point-${index}`}
               className='text-white-100 text-[14px] pl-1 tracking-wider'
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,9 +8,9 @@ import { fadeIn, textVariant } from '../utils/motion';
 import { TangentSpaceNormalMap } from 'three';
 import { p } from 'framer-motion/client';
 
-const ProjectCard = ({index, project}) => {
+const ProjectCard = ({index, name, description, tags, image, source_code_link}) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div>
       <Tilt 
         options={{
           max: 45,
@@ -20,13 +20,13 @@ const ProjectCard = ({index, project}) => {
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
-          <img src={project.image} alt={project.name}
+          <img src={image} alt={name}
             className='w-full h-full object-cover rounded-2xl'
           />
 
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
               <div
-                onClick={() => window.open(project.source_code_link, "_blank")}
+                onClick={() => window.open(source_code_link, "_blank")}
                 className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
               >
                 <img src={github} alt="github"
@@ -37,12 +37,12 @@ const ProjectCard = ({index, project}) => {
         </div>
 
         <div className='mt-5'>
-          <h3 className='text-white font-bold text-[24px]'>{project.name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{project.description}</p>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
         </div>
 
         <div className='mt-4 flex flex-wrap gap-2'>
-            {project.tags.map((tag) => (
+            {tags.map((tag) => (
               <p key={tag.name} className={`text-[14px] ${tag.color}`}>
                 #{tag.name}
               </p>
@@ -56,31 +56,35 @@ const ProjectCard = ({index, project}) => {
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div>
         <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
+      <div className='w-full flex flex-wrap'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+        
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]' 
         >
-
+          The following projects showcase my skills and experience. Each project is briefly described with links to the project. It reflects my ability to solve problems
+           and work with different technologies.
         </motion.p>
+      </div>
 
         <div className='mt-20 flex flex-wrap gap-7'>
           {projects.map((project, index) => (
             <ProjectCard 
             key={`project-${index}`} 
             index={index}
-            project={project}
+            {...project}
         
             />
           ))}
 
         </div>
-      </div>
+      
+
+      
     </>
   )
 }
